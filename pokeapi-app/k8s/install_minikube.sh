36c7cfb2f41be71c39b4450b91d9408e653fb74e
Vadim Sohin , Senior DevOps  Engineer <vadim.sohin@gmail.com>
2025-05-01 18:09:38 +0300

    Create install_minikube.sh

---
+++ pokeapi-app/k8s/install_minikube.sh
@@ -0,0 +1,12 @@
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+minikube start
+# to start with docke driver 
+minikube start --driver=docker
+# check if minikube working
+kubectl get nodes
